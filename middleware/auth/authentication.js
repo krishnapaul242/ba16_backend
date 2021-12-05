68cefb84bdf4ca5f9b994c1d7ecc40991efb3f2d
zahinafsar <afsarzahin@gmail.com>
2021-12-05 13:32:19 +0600

    Bug Fixes and Added some  Development Config

--- middleware/auth/authentication.js
+++ middleware/auth/authentication.js
@@ -6,143 +6,151 @@ const userModel = require('../../models/user');
 const { compare_date_time, get_current_date_time } = require('../../shared/datetime');
 
 exports.isAdminAuthenticated = async (req, res, next) => {
-    try {
-        if (req.headers.authorization) {
-            const token = req.headers.authorization.split(" ")[1];
-            if (token && token != ' ') {
-                jwt.verify(token, config.SECRECT_KEY.JWT_KEY, async (err, decoded) => {
-                    try {
-                        if (err) {
-                            return res.status(401).json({
-                                message: "Unauthorized",
-                                status: 0,
-                            });
-                        } else {
-                            let { iss, ag, sub, exp, isAdmin } = decoded.data;
-                            if (!isAdmin) {
-                                return res.status(401).json({
-                                    message: "Unauthorized",
-                                    status: 0,
-                                });
-                            }
-                            ag = decrypt_data(ag, config.SECRECT_KEY.CRYPTO_KEY,);
-                            if (iss != config.API_DETAILS.NAME) {
-                                return res.status(401).json({
-                                    message: "Unauthorized",
-                                    status: 0,
-                                });
-                            }
-                            if (ag != req.get("User-Agent")) {
+    if (process.env.NODE_ENV === 'testing') {
+        next();
+    } else {
+        try {
+            if (req.headers.authorization) {
+                const token = req.headers.authorization.split(" ")[1];
+                if (token && token != ' ') {
+                    jwt.verify(token, config.SECRECT_KEY.JWT_KEY, async (err, decoded) => {
+                        try {
+                            if (err) {
                                 return res.status(401).json({
                                     message: "Unauthorized",
                                     status: 0,
                                 });
                             } else {
-                                const comareDateTime = compare_date_time(get_current_date_time(), exp);
-                                if (comareDateTime == 1) {
+                                let { iss, ag, sub, exp, isAdmin } = decoded.data;
+                                if (!isAdmin) {
                                     return res.status(401).json({
                                         message: "Unauthorized",
                                         status: 0,
                                     });
                                 }
-                                sub = decrypt_data(sub, config.SECRECT_KEY.CRYPTO_KEY);
-                                const user = await adminModel.admin_details(sub);
-                                if (user.length > 0) {
-                                    req.user = user[0];
-                                    next();
-                                } else {
+                                ag = decrypt_data(ag, config.SECRECT_KEY.CRYPTO_KEY,);
+                                if (iss != config.API_DETAILS.NAME) {
                                     return res.status(401).json({
                                         message: "Unauthorized",
                                         status: 0,
                                     });
+                                }
+                                if (ag != req.get("User-Agent")) {
+                                    return res.status(401).json({
+                                        message: "Unauthorized",
+                                        status: 0,
+                                    });
+                                } else {
+                                    const comareDateTime = compare_date_time(get_current_date_time(), exp);
+                                    if (comareDateTime == 1) {
+                                        return res.status(401).json({
+                                            message: "Unauthorized",
+                                            status: 0,
+                                        });
+                                    }
+                                    sub = decrypt_data(sub, config.SECRECT_KEY.CRYPTO_KEY);
+                                    const user = await adminModel.admin_details(sub);
+                                    if (user.length > 0) {
+                                        req.user = user[0];
+                                        next();
+                                    } else {
+                                        return res.status(401).json({
+                                            message: "Unauthorized",
+                                            status: 0,
+                                        });
+                                    }
                                 }
                             }
+                        } catch (err) {
+                            const error = new Error(err);
+                            next(error);
                         }
-                    } catch (err) {
-                        const error = new Error(err);
-                        next(error);
-                    }
+                    });
+                }
+            } else {
+                return res.status(401).json({
+                    message: "Unauthorized",
+                    status: 0,
                 });
             }
-        } else {
-            return res.status(401).json({
-                message: "Unauthorized",
-                status: 0,
-            });
+        } catch (err) {
+            const error = new Error(err);
+            next(error);
         }
-    } catch (err) {
-        const error = new Error(err);
-        next(error);
     }
 };
 
 exports.isUserAuthenticated = async (req, res, next) => {
-    try {
-        if (req.headers.authorization) {
-            const token = req.headers.authorization.split(" ")[1];
-            if (token && token != ' ') {
-                jwt.verify(token, config.SECRECT_KEY.JWT_KEY, async (err, decoded) => {
-                    try {
-                        if (err) {
-                            return res.status(401).json({
-                                message: "Unauthorized",
-                                status: 0,
-                            });
-                        } else {
-                            let { iss, ag, sub, exp, isAdmin } = decoded.data;
-                            if (isAdmin) {
-                                return res.status(401).json({
-                                    message: "Unauthorized",
-                                    status: 0,
-                                });
-                            }
-                            ag = decrypt_data(ag, config.SECRECT_KEY.CRYPTO_KEY,);
-                            if (iss != config.API_DETAILS.NAME) {
-                                return res.status(401).json({
-                                    message: "Unauthorized",
-                                    status: 0,
-                                });
-                            }
-                            if (ag != req.get("User-Agent")) {
+    if (process.env.NODE_ENV === 'testing') {
+        next();
+    } else {
+        try {
+            if (req.headers.authorization) {
+                const token = req.headers.authorization.split(" ")[1];
+                if (token && token != ' ') {
+                    jwt.verify(token, config.SECRECT_KEY.JWT_KEY, async (err, decoded) => {
+                        try {
+                            if (err) {
                                 return res.status(401).json({
                                     message: "Unauthorized",
                                     status: 0,
                                 });
                             } else {
-                                const comareDateTime = compare_date_time(get_current_date_time(), exp);
-                                if (comareDateTime == 1) {
+                                let { iss, ag, sub, exp, isAdmin } = decoded.data;
+                                if (isAdmin) {
                                     return res.status(401).json({
                                         message: "Unauthorized",
                                         status: 0,
                                     });
                                 }
-                                sub = decrypt_data(sub, config.SECRECT_KEY.CRYPTO_KEY);
-                                const user = await userModel.get_user_wrt_id(sub);
-                                if (user.length > 0) {
-                                    req.user = user[0];
-                                    next();
-                                } else {
+                                ag = decrypt_data(ag, config.SECRECT_KEY.CRYPTO_KEY,);
+                                if (iss != config.API_DETAILS.NAME) {
                                     return res.status(401).json({
                                         message: "Unauthorized",
                                         status: 0,
                                     });
+                                }
+                                if (ag != req.get("User-Agent")) {
+                                    return res.status(401).json({
+                                        message: "Unauthorized",
+                                        status: 0,
+                                    });
+                                } else {
+                                    const comareDateTime = compare_date_time(get_current_date_time(), exp);
+                                    if (comareDateTime == 1) {
+                                        return res.status(401).json({
+                                            message: "Unauthorized",
+                                            status: 0,
+                                        });
+                                    }
+                                    sub = decrypt_data(sub, config.SECRECT_KEY.CRYPTO_KEY);
+                                    const user = await userModel.get_user_wrt_id(sub);
+                                    if (user.length > 0) {
+                                        req.user = user[0];
+                                        next();
+                                    } else {
+                                        return res.status(401).json({
+                                            message: "Unauthorized",
+                                            status: 0,
+                                        });
+                                    }
                                 }
                             }
+                        } catch (err) {
+                            const error = new Error(err);
+                            next(error);
                         }
-                    } catch (err) {
-                        const error = new Error(err);
-                        next(error);
-                    }
+                    });
+                }
+            } else {
+                return res.status(401).json({
+                    message: "Unauthorized",
+                    status: 0,
                 });
             }
-        } else {
-            return res.status(401).json({
-                message: "Unauthorized",
-                status: 0,
-            });
+        } catch (err) {
+            const error = new Error(err);
+            next(error);
         }
-    } catch (err) {
-        const error = new Error(err);
-        next(error);
     }
 };
--- routes/order.js
+++ routes/order.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 
 router.route('/').post(isUserAuthenticated, validateBody(product_order), orderController.add_order);
-router.route('/check_order').post(validateBody(check_order), orderController.check_order);
+router.route('/check_order').post(isUserAuthenticated, validateBody(check_order), orderController.check_order);
 router.route('/order_status').put(isAdminAuthenticated, orderController.update_order_status);
 router.route('/payment_status').put(isAdminAuthenticated, orderController.update_payment_status);
 router.route('/:order_status').get(isAdminAuthenticated, orderController.get_orders);
